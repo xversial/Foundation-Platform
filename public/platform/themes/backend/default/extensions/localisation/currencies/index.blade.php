9dc67ed8cd82de808f0cc302d0ee54c421301c66
bruno-g <brunofgaspar@live.com.pt>
2012-10-24 01:04:50 +0100

    Localisation views search filter fix

--- public/platform/themes/backend/default/extensions/localisation/currencies/index.blade.php
+++ public/platform/themes/backend/default/extensions/localisation/currencies/index.blade.php
@@ -42,6 +42,10 @@
                 </div>
             </div>
 
+            <div id="table-filters-applied"></div>
+
+            <hr>
+
             <div class="tabbable tabs-right">
                 <ul id="table-pagination" class="nav nav-tabs"></ul>
                 <div class="tab-content">
