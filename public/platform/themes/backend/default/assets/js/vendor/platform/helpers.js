0239ba56cdb969dbd13f7c1c1554dfa3bff234e4
Daniel Petrie <dan.petrie@cartalyst.com>
2012-11-13 12:20:10 -0800

    fixing slugify issue

--- public/platform/themes/backend/default/assets/js/vendor/platform/helpers.js
+++ public/platform/themes/backend/default/assets/js/vendor/platform/helpers.js
@@ -1,51 +1,55 @@
 // Tiny plugin to serialise an object
-$.fn.serializeObject = function() {
+(function($) {
+	$.fn.serializeObject = function() {
 
-	var o = {};
-	var a = this.serializeArray();
-	$.each(a, function() {
-		if (o[this.name] !== undefined) {
-			if ( ! o[this.name].push) {
-				o[this.name] = [o[this.name]];
+		var o = {};
+		var a = this.serializeArray();
+		$.each(a, function() {
+			if (o[this.name] !== undefined) {
+				if ( ! o[this.name].push) {
+					o[this.name] = [o[this.name]];
+				}
+				o[this.name].push(this.value || '');
+			} else {
+				o[this.name] = this.value || '';
 			}
-			o[this.name].push(this.value || '');
-		} else {
-			o[this.name] = this.value || '';
-		}
-	});
-	return o;
-};
+		});
+		return o;
+	};
+})(jQuery);
 
 // Function taken from http://dense13.com/blog/2009/05/03/converting-string-to-slug-javascript/
 // Some extra special characters added.
-// $.fn.slugify = function(separator) {
+(function($) {
+	$.fn.slugify = function(separator) {
 
-// 	if (typeof separator === 'undefined') {
-// 		separator = '-';
-// 	}
+		if (typeof separator === 'undefined') {
+			separator = '-';
+		}
 
-// 	// Get value attribute
-// 	var str = this;
-// 	if (typeof this !== 'string') {
-// 		var str = this.val();
-// 	}
+		// Get value attribute
+		var str = this;
+		if (typeof this !== 'string') {
+			var str = this.val();
+		}
 
-// 	str = str.replace(/^\s+|\s+$/g, ''); // trim
-// 	str = str.toLowerCase();
+		str = str.replace(/^\s+|\s+$/g, ''); // trim
+		str = str.toLowerCase();
 
-// 	// remove accents, swap ñ for n, etc
-// 	var from = "ĺěščřžýťňďàáäâèéëêìíïîòóöôùůúüûñç·/_,:;";
-// 	var to   = "lescrzytndaaaaeeeeiiiioooouuuuunc------";
-// 	for (var i=0, l=from.length ; i<l ; i++) {
-// 		str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-// 	}
+		// remove accents, swap ñ for n, etc
+		var from = "ĺěščřžýťňďàáäâèéëêìíïîòóöôùůúüûñç·/_,:;";
+		var to   = "lescrzytndaaaaeeeeiiiioooouuuuunc------";
+		for (var i=0, l=from.length ; i<l ; i++) {
+			str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+		}
 
-// 	return str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-// 				          .replace(/\s+/g, separator) // collapse whitespace and replace by _
-// 				          .replace(/-+/g, separator) // collapse dashes
-// 				          .replace(new RegExp(separator+'+$'), '') // Trim separator from start
-// 				          .replace(new RegExp('^'+separator+'+'), ''); // Trim separator from end
-// }
+		return str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+					          .replace(/\s+/g, separator) // collapse whitespace and replace by _
+					          .replace(/-+/g, separator) // collapse dashes
+					          .replace(new RegExp(separator+'+$'), '') // Trim separator from start
+					          .replace(new RegExp('^'+separator+'+'), ''); // Trim separator from end
+	}
+})(jQuery);
 
 // Confirmation Modal
 // Shows a confirmation modal on click
@@ -53,11 +57,11 @@ $.fn.serializeObject = function() {
 
 	$('table').on('click', '#modal-confirm', function(e) {
 		e.preventDefault();
-		console.log($(this).attr('href'));
 
 		$('#platform-modal-confirm .confirm').attr('href', $(this).attr('href'));
 		$('#platform-modal-confirm').modal({show:true});
 
 		return false;
 	});
+
 })(jQuery);
