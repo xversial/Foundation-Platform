d765929e6c17d873d06e622a689c0947a88a1330
drsii <dan.syme@gmail.com>
2012-12-27 10:27:54 -0800

    prepping to make platform themes easier to understand.
    Signed-off-by: drsii <dan.syme@gmail.com>

---
+++ public/platform/themes/frontend/default/templates/layouts/narrow.blade.php
@@ -0,0 +1,25 @@
+@layout('extensions.platform.pages.narrow')
+
+<!-- Title -->
+@section('title')
+	@get('platform/settings::site.title')
+@endsection
+
+<!-- Queue Styles | e.g Theme::queue_asset('name', 'path_to_css', 'dependency') -->
+
+<!-- Styles -->
+@section ('styles')
+@endsection
+
+<!-- Queue Scripts | e.g. Theme::queue_asset('name', 'path_to_js', 'dependency') -->
+
+<!-- Scripts -->
+@section('scripts')
+@endsection
+
+<!-- Content -->
+@section('content')
+
+	{{ $content }}
+
+@endsection
