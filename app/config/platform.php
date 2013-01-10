32cddb2d4af31da06a293b90c2dd86328b30bde3
Ben Corlett <bencorlett@me.com>
2013-01-10 14:55:04 +1100

    Adding user configurable Platform configuration.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

---
+++ app/config/platform.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+
+	'site' => array(
+
+		/*
+		|--------------------------------------------------------------------------
+		| Site Title
+		|--------------------------------------------------------------------------
+		|
+		| Here you may specify the title of the site you are building, to be used
+		| throughout the templates (as an example).
+		|
+		*/
+
+		'title' => 'Platform',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Site Tagline
+		|--------------------------------------------------------------------------
+		|
+		| If your site has a tagline feel free to specify it here.
+		|
+		*/
+
+		'tagline' => 'An application framework build on Laravel',
+
+	),
+
+);
