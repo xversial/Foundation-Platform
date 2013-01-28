55886e1fe8aad2557bf42fdf9103d6ad63a007e7
Bruno G <brunofgaspar1@gmail.com>
2013-01-28 21:59:19 +0000

    media extension views updates

--- public/platform/themes/admin/platform/default/extensions/platform/media/views/index.blade.php
+++ public/platform/themes/admin/platform/default/extensions/platform/media/views/index.blade.php
@@ -23,5 +23,64 @@
 	</h3>
 </div>
 
-
+<table class="table table-bordered">
+	<thead>
+		<tr>
+			<th>Id</th>
+			<th>Name</th>
+			<th class="span2">File Extension</th>
+			<th class="span2">Mime Type</th>
+			<th class="span2">Size</th>
+			<th class="span2">Width</th>
+			<th class="span2">Height</th>
+			<th class="span2">{{ Lang::get('table.actions') }}</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach ($media as $row)
+			<tr>
+				<td class="span1">
+					{{ $row->id }}
+				</td>
+				<td>
+					{{ $row->name }}
+				</td>
+				<td>
+					{{ $row->file_extension }}
+				</td>
+				<td>
+					{{ $row->file_mime_type }}
+				</td>
+				<td>
+					{{ $row->file_size() }}
+				</td>
+				<td>
+					{{ $row->width() }}
+				</td>
+				<td>
+					{{ $row->height() }}
+				</td>
+				<td>
+					<div class="btn-group">
+						<a href="{{ URL::to(ADMIN_URI . "/media/view/{$row->id}") }}" class="btn btn-mini">
+							{{ Lang::get('button.edit') }}
+						</a>
+						<a href="{{ URL::to(ADMIN_URI . "/media/delete/{$row->id}") }}" class="btn btn-mini btn-danger">
+							{{ Lang::get('button.delete') }}
+						</a>
+					</div>
+				</td>
+			</tr>
+		@endforeach
+	</tbody>
+	<tfoot>
+		<tr>
+			<td colspan="6">
+				<div class="pull-right">
+					{{ $media->links() }}
+				</div>
+			</td>
+		</tr>
+	</tfoot>
+</table>
 @stop
