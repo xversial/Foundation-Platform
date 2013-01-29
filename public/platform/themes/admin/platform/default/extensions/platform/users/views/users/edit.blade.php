e48af29c921405924400ac8c279008f07912b73a
Bruno G <brunofgaspar1@gmail.com>
2013-01-29 04:40:39 +0000

    users extension edit view update
    permissions work

--- public/platform/themes/admin/platform/default/extensions/platform/users/views/users/edit.blade.php
+++ public/platform/themes/admin/platform/default/extensions/platform/users/views/users/edit.blade.php
@@ -29,93 +29,123 @@
 	<!-- CSRF Token -->
 	<input type="hidden" name="csrf_token" value="{{ csrf_token() }}">
 
-	<!-- First Name -->
-	<div class="control-group">
-		<label class="control-label" for="first_name">{{ Lang::get('platform/users::users/form.first_name') }}:</label>
-		<div class="controls">
-			<div class="input-append">
-				<input type="text" name="first_name" id="first_name" value="{{ Input::old('first_name', $user->first_name) }}" placeholder="{{ Lang::get('platform/users::users/form.first_name') }}" required>
-				<span class="add-on"><i class="icon-user"></i></span>
-			</div>
-			<span class="help-block">{{ Lang::get('platform/users::users/form.first_name_help') }}</span>
-		</div>
-	</div>
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#tab-general" data-toggle="tab">General</a></li>
+		<li><a href="#tab-permissions" data-toggle="tab">Permissions</a></li>
+	</ul>
 
-	<!-- Last Name -->
-	<div class="control-group">
-		<label class="control-label" for="last_name">{{ Lang::get('platform/users::users/form.last_name') }}:</label>
-		<div class="controls">
-			<div class="input-append">
-				<input type="text" name="last_name" id="last_name" value="{{ Input::old('last_name', $user->last_name) }}" placeholder="{{ Lang::get('platform/users::users/form.last_name') }}" required>
-				<span class="add-on"><i class="icon-user"></i></span>
-			</div>
-			<span class="help-block">{{ Lang::get('platform/users::users/form.last_name_help') }}</span>
-		</div>
-	</div>
+ 	<div class="tab-content">
+ 		<!-- Tab General -->
+		<div class="tab-pane active" id="tab-general">
 
-	<!-- Email Address -->
-	<div class="control-group">
-		<label class="control-label" for="email">{{ Lang::get('platform/users::users/form.email') }}:</label>
-		<div class="controls">
-			<div class="input-append">
-				<input type="email" name="email" id="email" value="{{ Input::old('email', $user->email) }}" placeholder="{{ Lang::get('platform/users::users/form.email') }}" required>
-				<span class="add-on"><i class="icon-envelope"></i></span>
+			<!-- First Name -->
+			<div class="control-group">
+				<label class="control-label" for="first_name">{{ Lang::get('platform/users::users/form.first_name') }}:</label>
+				<div class="controls">
+					<div class="input-append">
+						<input type="text" name="first_name" id="first_name" value="{{ Input::old('first_name', $user->first_name) }}" placeholder="{{ Lang::get('platform/users::users/form.first_name') }}" required>
+						<span class="add-on"><i class="icon-user"></i></span>
+					</div>
+					<span class="help-block">{{ Lang::get('platform/users::users/form.first_name_help') }}</span>
+				</div>
 			</div>
-			<span class="help-block">{{ Lang::get('platform/users::users/form.email_help') }}</span>
-		</div>
-	</div>
 
-	<!-- Password -->
-	<div class="control-group">
-		<label class="control-label" for="password">{{ Lang::get('platform/users::users/form.password') }}:</label>
-		<div class="controls">
-			<div class="input-append">
-				<input type="password" name="password" id="password" value="{{ Input::old('password') }}" placeholder="{{ Lang::get('platform/users::users/form.password') }}">
-				<span class="add-on"><i class="icon-key"></i></span>
+			<!-- Last Name -->
+			<div class="control-group">
+				<label class="control-label" for="last_name">{{ Lang::get('platform/users::users/form.last_name') }}:</label>
+				<div class="controls">
+					<div class="input-append">
+						<input type="text" name="last_name" id="last_name" value="{{ Input::old('last_name', $user->last_name) }}" placeholder="{{ Lang::get('platform/users::users/form.last_name') }}" required>
+						<span class="add-on"><i class="icon-user"></i></span>
+					</div>
+					<span class="help-block">{{ Lang::get('platform/users::users/form.last_name_help') }}</span>
+				</div>
 			</div>
-			<span class="help-block">{{ Lang::get('platform/users::users/form.password_help') }}</span>
-		</div>
-	</div>
 
-	<!-- Password Confirmation -->
-	<div class="control-group">
-		<label class="control-label" for="password_confirmation">{{ Lang::get('platform/users::users/form.password_confirm') }}:</label>
-		<div class="controls">
-			<div class="input-append">
-				<input type="password" name="password_confirmation" id="password_confirmation" value="{{ Input::old('password_confirmation') }}" placeholder="{{ Lang::get('platform/users::users/form.password_confirm') }}">
-				<span class="add-on"><i class="icon-key"></i></span>
+			<!-- Email Address -->
+			<div class="control-group">
+				<label class="control-label" for="email">{{ Lang::get('platform/users::users/form.email') }}:</label>
+				<div class="controls">
+					<div class="input-append">
+						<input type="email" name="email" id="email" value="{{ Input::old('email', $user->email) }}" placeholder="{{ Lang::get('platform/users::users/form.email') }}" required>
+						<span class="add-on"><i class="icon-envelope"></i></span>
+					</div>
+					<span class="help-block">{{ Lang::get('platform/users::users/form.email_help') }}</span>
+				</div>
 			</div>
-			<span class="help-block">{{ Lang::get('platform/users::users/form.password_confirm_help') }}</span>
-		</div>
-	</div>
 
-	<!-- Groups -->
-	<div class="control-group">
-		<label class="control-label" for="groups">{{ Lang::get('platform/users::users/form.groups') }}:</label>
-		<div class="controls">
-			<div class="input-append">
-				<select name="groups[]" id="groups[]" multiple="multiple">
-					@foreach ($groups as $groupId => $groupName)
-					<option value="{{ $groupId }}"{{ (array_key_exists($groupId, $userGroups) ? ' selected="selected"' : '') }}>{{ $groupName }}</option>
-					@endforeach
-				</select>
+			<!-- Password -->
+			<div class="control-group">
+				<label class="control-label" for="password">{{ Lang::get('platform/users::users/form.password') }}:</label>
+				<div class="controls">
+					<div class="input-append">
+						<input type="password" name="password" id="password" value="{{ Input::old('password') }}" placeholder="{{ Lang::get('platform/users::users/form.password') }}">
+						<span class="add-on"><i class="icon-key"></i></span>
+					</div>
+					<span class="help-block">{{ Lang::get('platform/users::users/form.password_help') }}</span>
+				</div>
 			</div>
-			<span class="help-block">{{ Lang::get('platform/users::users/form.groups_help') }}</span>
-		</div>
-	</div>
 
-	<!-- Activation Status -->
-	<div class="control-group">
-		<label class="control-label" for="activated">{{ Lang::get('platform/users::users/form.activated') }}</label>
-		<div class="controls">
-			<div class="input-append">
-				<select name="activated" id="activated" required>
-					<option value="1"{{ ($user->isActivated() ? ' selected="selected"' : '') }}>{{ Lang::get('general.yes') }}</option>
-					<option value="0"{{ ( ! $user->isActivated() ? ' selected="selected"' : '') }}>{{ Lang::get('general.no') }}</option>
-				</select>
+			<!-- Password Confirmation -->
+			<div class="control-group">
+				<label class="control-label" for="password_confirmation">{{ Lang::get('platform/users::users/form.password_confirm') }}:</label>
+				<div class="controls">
+					<div class="input-append">
+						<input type="password" name="password_confirmation" id="password_confirmation" value="{{ Input::old('password_confirmation') }}" placeholder="{{ Lang::get('platform/users::users/form.password_confirm') }}">
+						<span class="add-on"><i class="icon-key"></i></span>
+					</div>
+					<span class="help-block">{{ Lang::get('platform/users::users/form.password_confirm_help') }}</span>
+				</div>
 			</div>
-			<span class="help-block">{{ Lang::get('platform/users::users/form.activated_help') }}</span>
+
+			<!-- Groups -->
+			<div class="control-group">
+				<label class="control-label" for="groups">{{ Lang::get('platform/users::users/form.groups') }}:</label>
+				<div class="controls">
+					<div class="input-append">
+						<select name="groups[]" id="groups[]" multiple="multiple">
+							@foreach ($groups as $groupId => $groupName)
+							<option value="{{ $groupId }}"{{ (array_key_exists($groupId, $userGroups) ? ' selected="selected"' : '') }}>{{ $groupName }}</option>
+							@endforeach
+						</select>
+					</div>
+					<span class="help-block">{{ Lang::get('platform/users::users/form.groups_help') }}</span>
+				</div>
+			</div>
+
+			<!-- Activation Status -->
+			<div class="control-group">
+				<label class="control-label" for="activated">{{ Lang::get('platform/users::users/form.activated') }}</label>
+				<div class="controls">
+					<div class="input-append">
+						<select name="activated" id="activated" required>
+							<option value="1"{{ ($user->isActivated() ? ' selected="selected"' : '') }}>{{ Lang::get('general.yes') }}</option>
+							<option value="0"{{ ( ! $user->isActivated() ? ' selected="selected"' : '') }}>{{ Lang::get('general.no') }}</option>
+						</select>
+					</div>
+					<span class="help-block">{{ Lang::get('platform/users::users/form.activated_help') }}</span>
+				</div>
+			</div>
+		 </div>
+		<!-- ./ tab-general -->
+
+		<!-- Tab Permissions -->
+		<div class="tab-panex" id="tab-permissions">
+ 			@foreach ($permissions as $extension => $perms)
+			<fieldset>
+				<legend>{{ $extension }}</legend>
+				@foreach ($perms as $permission)
+					<div>
+						<label>
+							<input type="checkbox" id="permissions[]" name="permissions[]" value="{{ $permission['slug'] }}"{{ (array_key_exists($permission['permission'], $userPermissions) ? ' checked="checked"' : '')}} />
+							{{ $permission['label'] }}
+						</label>
+					</div>
+				@endforeach
+			</fieldset>
+			@endforeach
 		</div>
+		<!-- ./ tab-permissions -->
 	</div>
 
 	<!-- Form Actions -->
