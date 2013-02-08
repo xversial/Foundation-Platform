fa52e11e04f8774e59598987aa42eaaa2ec97c67
drsii <dan.syme@gmail.com>
2013-02-07 16:51:22 -0800

    adds native support to default fronted theme for drop down menus.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- public/platform/themes/frontend/default/extensions/platform/menus/widgets/nav.blade.php
+++ public/platform/themes/frontend/default/extensions/platform/menus/widgets/nav.blade.php
@@ -1,3 +1,5 @@
+{{ Theme::queue_asset('bootstrap-dropdown', 'js/bootstrap/dropdown.js', 'jquery') }}
+
 <ul class="{{ $class }}">
 @foreach ($items as $item)
 	@render('platform/menus::widgets.nav.item', array('item' => $item, 'active_path' => $active_path, 'before_uri' => $before_uri))
--- public/platform/themes/frontend/default/extensions/platform/menus/widgets/nav/item.blade.php
+++ public/platform/themes/frontend/default/extensions/platform/menus/widgets/nav/item.blade.php
@@ -1,4 +1,24 @@
+@if ($item['children'])
+<li class="dropdown">
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		@if ($item['class'])
+			<i class="{{ $item['class'] }}"></i>
+		@endif
+		<span>
+			{{ $item['name'] }}
+		</span> <b class="caret"></b>
+	</a>
+
+	<ul class="dropdown-menu">
+		@foreach ($item['children'] as $child)
+			@render('platform/menus::widgets.nav.item', array('item' => $child, 'active_path' => $active_path, 'before_uri' => $before_uri))
+		@endforeach
+	</ul>
+
+
+@else
 <li class="{{ in_array($item['id'], $active_path) ? 'active' : null }}">
+
 	<a href="{{ $item['uri'] }}" target="{{ $item['target'] }}" title="{{ $item['name'] }}">
 		@if ($item['class'])
 			<i class="{{ $item['class'] }}"></i>
@@ -7,12 +27,6 @@
 			{{ $item['name'] }}
 		</span>
 	</a>
+@endif
 
-	@if ($item['children'])
-		<ul>
-			@foreach ($item['children'] as $child)
-				@render('platform/menus::widgets.nav.item', array('item' => $child, 'active_path' => $active_path, 'before_uri' => $before_uri))
-			@endforeach
-		</ul>
-	@endif
 </li>
