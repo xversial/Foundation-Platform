5eb7538a8be859a8aa1bc1dc8b5ec2ad3230c88a
Bruno G. <brunofgaspar1@gmail.com>
2013-02-25 20:24:31 +0000

    fix menu bug

--- public/platform/themes/backend/default/extensions/platform/menus/edit/child/static.blade.php
+++ public/platform/themes/backend/default/extensions/platform/menus/edit/child/static.blade.php
@@ -2,6 +2,6 @@
 <div class="control-group">
 	<label class="control-label" for="menu-children-{{ array_get($child, 'id', '[%id%]') }}-uri">{{ Lang::line('platform/menus::form.child.uri') }}</label>
 	<div class="controls">
-		<input type="text" name="children[{{ array_get($child, 'id', '[%id%]') }}][uri]" id="menu-children-{{ array_get($child, 'id', '[%id%]') }}-uri" class="child-uri" value="{{ array_get($child, 'uri', '[%control.uri%]') }}" {{ (array_key_exists('uri', $child) and ( ! array_get($child, 'user_editable') or URL::valid(array_get($child, 'uri')))) ? 'disabled' : 'required' }}>
+		<input type="text" name="children[{{ array_get($child, 'id', '[%id%]') }}][uri]" id="menu-children-{{ array_get($child, 'id', '[%id%]') }}-uri" class="child-uri" value="{{ array_get($child, 'uri', '[%control.uri%]') }}" {{ (array_key_exists('uri', $child) and ( ! array_get($child, 'user_editable'))) ? 'disabled' : 'required' }}>
 	</div>
 </div>
