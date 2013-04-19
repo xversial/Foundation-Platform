d748933b077eac5eae972584ac15086a6efd2d96
Ben Corlett <bencorlett@me.com>
2013-04-19 18:06:35 +1000

    Tweak notifications (errors & success messages).
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- public/platform/themes/admin/platform/default/views/notifications.blade.php
+++ public/platform/themes/admin/platform/default/views/notifications.blade.php
@@ -1,31 +1,33 @@
-@if ($errors->any())
-<div class="alert alert-error alert-block">
-	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Error</h4>
-	@if ($message = $errors->first(0, ':message'))
-	{{ $message }}
-	@else
-	Please check the form below for errors
+<section id="notifications">
+	<br>
+
+	@if ($errors->any())
+		<div class="alert alert-error alert-block">
+			<!-- <button type="button" class="close" data-dismiss="alert">&times;</button> -->
+			@if ($message = $errors->first(0, ':message'))
+				{{ $message }}
+			@else
+				Please check the form below for errors
+			@endif
+		</div>
 	@endif
-</div>
-@endif
 
-@if ($messages = Session::get('messages') and $messages->has('success'))
-<div class="alert alert-success alert-block">
-	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Success</h4>
-	@foreach ($messages->get('success') as $message)
-	{{ $message }}
-	@endforeach
-</div>
-@endif
+	@if ($messages = Session::get('messages') and $messages->has('success'))
+		@foreach ($messages->get('success') as $message)
+			<div class="alert alert-success alert-block">
+				<!-- <button type="button" class="close" data-dismiss="alert">&times;</button> -->
+				{{ $message }}
+			</div>
+		@endforeach
+	@endif
 
-@if ($messages = Session::get('messages') and $messages->has('error'))
-<div class="alert alert-error alert-block">
-	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Error</h4>
-	@foreach ($messages->get('error') as $message)
-	{{ $message }}
-	@endforeach
-</div>
-@endif
+	@if ($messages = Session::get('messages') and $messages->has('error'))
+		@foreach ($messages->get('error') as $message)
+			<div class="alert alert-error alert-block">
+				<!-- <button type="button" class="close" data-dismiss="alert">&times;</button> -->
+				{{ $message }}
+			</div>
+		@endforeach
+	@endif
+
+</section>
