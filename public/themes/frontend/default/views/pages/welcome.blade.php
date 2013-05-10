a47aa89ec9de33b5fef90bbc2b605fb3538a0566
Ben Corlett <bencorlett@me.com>
2013-05-10 19:03:38 +1000

    Referencing default content from default page.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- public/themes/frontend/default/views/pages/welcome.blade.php
+++ public/themes/frontend/default/views/pages/welcome.blade.php
@@ -4,6 +4,12 @@
 
 <h1>This is the default template</h1>
 
+<div class="well">
+	@content('welcome')
+</div>
+
+<h3>Now, back to the page</h3>
+
 <p>It's located at <code>pages/welcome.blade.php</code> of your theme.</p>
 
 <a href="{{ URL::toAdmin('/') }}" class="btn">Admin</a>
