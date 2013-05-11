e819ce784c8c907e9dcde37d8f048c4838cb5509
Bruno G. <brunofgaspar1@gmail.com>
2013-05-11 05:34:00 +0100

    add platform/operations config

---
+++ app/config/packages/platform/operations/config.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Part of the Platform application.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Platform
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Trusted IPs
+	|--------------------------------------------------------------------------
+	|
+	| Here, you may add entries for IP addresses who are allowed to undertake
+	| operations (such as installing and updating) through the web interface.
+	|
+	| We accept hard-coded IP addresses, partial or full wildcard "*" entries.
+	|
+	*/
+
+	'trusted_ips' => array(
+
+		'127.0.0.1',
+
+		// // Allow all IP address'
+		// '*',
+
+	),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Elligibility Whitelist
+	|--------------------------------------------------------------------------
+	|
+	| When Platform is not installed, if the trusted IP address filter passes,
+	| the following URIs can be accessed from your application. Feel free to
+	| add more if you have your own installation logic.
+	|
+	*/
+
+	'eligibility_whitelist' => array(
+
+		'installer',
+		'installer/*',
+
+	),
+
+);
