d23bb3c983f740887aa6ab645c5d80f0a583abe0
Bruno G. <brunofgaspar1@gmail.com>
2013-05-29 20:07:49 +0100

    fix modal windows

--- public/themes/admin/default/views/modals.blade.php
+++ public/themes/admin/default/views/modals.blade.php
@@ -1,4 +1,5 @@
- {{ Asset::queue('platform-helpers', 'js/vendor/platform/helpers.js', 'bootstrap-modal') }}
+{{ Asset::queue('platform-helpers', 'js/vendor/platform/helpers.js', 'bootstrap-modal') }}
+{{ Asset::queue('modal', 'js/vendor/bootstrap/modal.js', 'jquery') }}
 
 <div id="platform-modal-confirm" class="modal hide fade">
 
@@ -11,18 +12,13 @@
 
 	<div class="modal-body">
 
-		<h4>You are about to delete this record, Continue?</h4>
-
 		<h4>You are about to delete this record, do you want to Continue?</h4>
 
 	</div>
 
 	<div class="modal-footer">
-
 		<button class="btn btn-cancel" data-dismiss="modal" aria-hidden="true"><i class="icon-circle-arrow-left"></i> @lang('button.cancel')</button>
-		<a href="#" class="btn btn-primary confirm"><i class="icon-trash"></i> @lang('button.delete')</a>
 		<a href="#" class="btn btn-primary confirm"><i class="icon-trash"></i> @lang('button.delete') </a>
-
 	</div>
 
 </div>
