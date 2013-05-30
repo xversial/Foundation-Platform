85383d2e94822d6f22f94a8eebe5c67418d32545
drsii <dan.syme@gmail.com>
2013-05-30 10:31:56 -0700

    oh my god, our base color system rocks.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- public/themes/admin/default/views/templates/default.blade.php
+++ public/themes/admin/default/views/templates/default.blade.php
@@ -23,7 +23,7 @@
 		{{ Asset::queue('jquery', 'js/vendor/jquery/jquery.js') }}
 		{{ Asset::queue('tooltip', 'js/vendor/bootstrap/tooltip.js', 'jquery') }}
 		{{ Asset::queue('helpers', 'js/vendor/platform/helpers.js', array('jquery')) }}
-		{{ Asset::queue('script', 'js/script.js', array('jquery')) }}
+		{{ Asset::queue('script', 'js/script.js', array('tooltip')) }}
 
 		{{-- Call partial assets --}}
 		@section('assets')
