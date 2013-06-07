647a46ba1839e1813d601ae2a55f4df13ff2c298
drsii <dan.syme@gmail.com>
2013-06-07 10:00:51 -0500

    updated notifications to be prettier.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- public/themes/frontend/default/views/partials/notifications.blade.php
+++ public/themes/frontend/default/views/partials/notifications.blade.php
@@ -1,31 +1,40 @@
 @if ($errors->any())
 <div class="alert alert-error alert-block">
-	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Error</h4>
-	@if ($message = $errors->first(0, ':message'))
-	{{ $message }}
-	@else
-	Please check the form below for errors
-	@endif
+	<button type="button" class="close" data-dismiss="alert"><i class="icon-collapse-alt"></i></button>
+	<h4>Error
+		<span>
+			@if ($message = $errors->first(0, ':message'))
+				{{ $message }}
+			@else
+				Please check the form below for errors
+			@endif
+		</span>
+	</h4>
 </div>
 @endif
 
 @if ($notifications = Session::get('notifications') and $notifications->has('success'))
 <div class="alert alert-success alert-block">
-	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Success</h4>
-	@foreach ($notifications->get('success') as $message)
-	{{ $message }}
-	@endforeach
+	<button type="button" class="close" data-dismiss="alert"><i class="icon-collapse-alt"></i></button>
+	<h4>Success
+		<span>
+			@foreach ($notifications->get('success') as $message)
+				{{ $message }}
+			@endforeach
+		</span>
+	</h4>
 </div>
 @endif
 
 @if ($notifications = Session::get('notifications') and $notifications->has('error'))
 <div class="alert alert-error alert-block">
-	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Error</h4>
-	@foreach ($notifications->get('error') as $message)
-	{{ $message }}
-	@endforeach
+	<button type="button" class="close" data-dismiss="alert"><i class="icon-collapse-alt"></i></button>
+	<h4>Error
+		<span>
+			@foreach ($notifications->get('error') as $message)
+				{{ $message }}
+			@endforeach
+		</span>
+	</h4>
 </div>
 @endif
