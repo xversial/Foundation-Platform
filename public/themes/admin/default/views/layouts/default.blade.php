13df0a8e3135827de0c023246b8b1a5853151bf3
Jared <jared@wearedifrnt.com>
2013-11-04 15:37:10 -0800

    Moved footer to a partial for better flexibility

--- public/themes/admin/default/views/layouts/default.blade.php
+++ public/themes/admin/default/views/layouts/default.blade.php
@@ -97,21 +97,7 @@
 		</div>
 
 		{{-- Footer --}}
-		<footer id="footer">
-
-			<div class="container">
-
-				<span>
-					<a href="{{ URL::to('http://www.cartalyst.com/licence') }}" target="_blank">
-						Copyright &copy; 2011 - 2013 Cartalyst LLC
-					</a>
-				</span>
-
-				<span class="pull-right">Platform Version {{{ PLATFORM_VERSION }}}</span>
-
-			</div>
-
-		</footer>
+		@include('partials/footer')
 
 		{{-- Modals --}}
 		@include('partials/modals')
---
+++ public/themes/admin/default/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<footer id="footer">
+
+	<div class="container">
+
+		<span>
+			<a href="{{ URL::to('http://www.cartalyst.com/licence') }}" target="_blank">
+				Copyright &copy; 2011 - 2013 Cartalyst LLC
+			</a>
+		</span>
+
+		<span class="pull-right">Platform Version {{{ PLATFORM_VERSION }}}</span>
+
+	</div>
+
+</footer>
