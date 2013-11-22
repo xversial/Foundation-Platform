c10d410e5c74ac4cd87a7436c546a0a210be119e
Bruno Gaspar <brunofgaspar1@gmail.com>
2013-11-22 04:53:06 +0000

    fix bootstrap themes notifications close icons
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- public/themes/admin/default/views/partials/notifications.blade.php
+++ public/themes/admin/default/views/partials/notifications.blade.php
@@ -2,7 +2,7 @@
 
 	<div class="alert alert-danger alert-block">
 
-		<button type="button" class="close" data-dismiss="alert"><i class="icon-collapse-alt"></i></button>
+		<button type="button" class="close" data-dismiss="alert"><i class="fa fa-minus-square"></i></button>
 
 		<strong>{{{ trans('general.error') }}}</strong>
 
@@ -20,7 +20,7 @@
 
 	<div class="alert alert-success alert-block">
 
-		<button type="button" class="close" data-dismiss="alert"><i class="icon-collapse-alt"></i></button>
+		<button type="button" class="close" data-dismiss="alert"><i class="fa fa-minus-square"></i></button>
 
 		<strong>{{{ trans('general.success') }}}</strong> {{ $message }}
 
