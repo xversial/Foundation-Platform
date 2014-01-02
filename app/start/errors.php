c83b0bc43e29299820e7018f22b0f8fd061bfb56
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-01-02 16:43:18 +0000

    simplify the error handler
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- app/start/errors.php
+++ app/start/errors.php
@@ -13,50 +13,12 @@
 |
 */
 
-if (App::environment() === 'production' or Config::get('app.debug') === false)
+App::error(function(Exception $exception, $code)
 {
-	App::error(function(Symfony\Component\HttpKernel\Exception\HttpException $exception, $code)
+	Log::error($exception);
+
+	if (App::environment() === 'production' or Config::get('app.debug') === false)
 	{
-		Log::error($exception);
-
-		return show_error_page($exception->getStatusCode());
-	});
-
-	App::error(function(Symfony\Component\HttpKernel\Exception\NotFoundHttpException $exception, $code)
-	{
-		Log::error($exception);
-
-		return show_error_page(404);
-	});
-
-	App::error(function(Cartalyst\Api\Http\ApiHttpException $exception, $code)
-	{
-		Log::error($exception);
-
-		// If the API is throwing a 404 status, we'll actually return a
-		// 500 error. The main reason for this, is that the app should
-		// be handling all possible API Exceptions that are thrown.
-		// If one bubbles back to the user, there is definitely
-		// an "internal server error".
-		if (($statusCode = $exception->getStatusCode()) == 404)
-		{
-			$statusCode = 500;
-		}
-
-		return show_error_page($statusCode);
-	});
-
-	App::error(function(Exception $exception, $code)
-	{
-		Log::error($exception);
-
-		return show_error_page(500);
-	});
-}
-else
-{
-	App::error(function(Exception $exception, $code)
-	{
-		Log::error($exception);
-	});
-}
+		return show_error_page($code);
+	}
+});
