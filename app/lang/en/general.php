8403340245cceffdb559ec057cc239eb7e8f832f
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-03-07 05:08:32 +0000

    update lang files
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- app/lang/en/general.php
+++ app/lang/en/general.php
@@ -30,11 +30,15 @@ return array(
 	'enabled'  => 'Enabled',
 	'disabled' => 'Disabled',
 
+	'actions'    => 'Actions',
+	'status'     => 'Status',
+	'no_results' => 'No Results',
+
 	'delete_record' => 'You are about to delete this record, do you want to continue?',
 
 	'not_logged_in' => 'You are not logged in.',
 
-	// Search & Pagination
+	// Search, Filter & Pagination
 	'showing'       => 'Showing',
 	'to'            => 'to',
 	'in'            => 'in',
@@ -42,7 +46,12 @@ return array(
 	'entries'       => 'entries',
 	'loading'       => 'Loading',
 	'search'        => 'Search',
-	'load_more'     => 'Load more',
+	'filters'       => 'Filters',
 	'remove_filter' => 'Remove filter',
+	'show_all'      => 'Show all',
+	'show_enabled'  => 'Show enabled',
+	'show_disabled' => 'Show disabled',
+	'all_enabled'   => 'All Enabled',
+	'all_disabled'  => 'All Disabled',
 
 );
