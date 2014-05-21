8f0fb7f16377a42dc17b57265e3a6c0322f7c70b
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-05-21 21:53:34 +0100

    update copyright headers

--- public/themes/admin/default/assets/platform/less/style.less
+++ public/themes/admin/default/assets/platform/less/style.less
@@ -1,18 +1,17 @@
 /**
- * Part of the Platform application.
+ * Part of the Platform package.
  *
  * NOTICE OF LICENSE
  *
- * Licensed under the 3-clause BSD License.
+ * Licensed under the Cartalyst PSL License.
  *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
  *
  * @package    Platform
  * @version    2.0.0
  * @author     Cartalyst LLC
- * @license    BSD License (3-clause)
+ * @license    Cartalyst PSL
  * @copyright  (c) 2011-2014, Cartalyst LLC
  * @link       http://cartalyst.com
  */
