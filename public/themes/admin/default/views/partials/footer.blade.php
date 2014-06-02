497142c332551d07bf3544538573fddf0a851797
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-06-02 16:31:56 +0100

    update admin theme license link on footer

--- public/themes/admin/default/views/partials/footer.blade.php
+++ public/themes/admin/default/views/partials/footer.blade.php
@@ -5,7 +5,7 @@
 		<div class="col-lg-12">
 
 			<span>
-				<a href="{{ URL::to('http://www.cartalyst.com/licence') }}" target="_blank">
+				<a href="{{ URL::to('http://www.cartalyst.com/license') }}" target="_blank">
 					Copyright &copy; 2011-2014 Cartalyst LLC
 				</a>
 			</span>
