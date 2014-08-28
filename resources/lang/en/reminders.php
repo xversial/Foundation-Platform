4b88c1eddb72a46654d090d98260f1d0a2d68d8c
serhatdurum <serhatdurum@gmail.com>
2014-08-28 13:08:05 +0300

    Convert language PHP 5.4 arrays.

--- resources/lang/en/reminders.php
+++ resources/lang/en/reminders.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * Part of the Platform application.
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the Cartalyst PSL License.
- *
- * This source file is subject to the Cartalyst PSL License that is
- * bundled with this package in the LICENSE file.
- *
- * @package    Platform
- * @version    2.0.0
- * @author     Cartalyst LLC
- * @license    Cartalyst PSL
- * @copyright  (c) 2011-2015, Cartalyst LLC
- * @link       http://cartalyst.com
- */
 
 return [
 
