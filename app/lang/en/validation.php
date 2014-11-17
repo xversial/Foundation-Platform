59c85191a418d862f389790afbb50c426c32b8bc
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-11-17 20:11:10 +0000

    fix validation language file
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -43,7 +43,7 @@ return [
 		"file"    => "The :attribute must be between :min and :max kilobytes.",
 		"string"  => "The :attribute must be between :min and :max characters.",
 		"array"   => "The :attribute must have between :min and :max items.",
-	),
+	],
 	"boolean"              => "The :attribute field must be true or false.",
 	"confirmed"            => "The :attribute confirmation does not match.",
 	"date"                 => "The :attribute is not a valid date.",
