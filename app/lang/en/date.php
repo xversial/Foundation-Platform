4b6507cf977de46ef2ce310a55dc7e75bd9cd7c3
drsii <dan.syme@gmail.com>
2014-12-18 16:52:57 -0800

    add date languages

---
+++ app/lang/en/date.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Part of the Platform application.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
+ *
+ * @package    Platform
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Date Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used throughout the whole application.
+	| You can change them to anything you want to customize your views to
+	| better match your application.
+	|
+	*/
+
+	'today'         => 'Today',
+	'tomorrow'      => 'Tomorrow',
+	'yesterday'     => 'Yesterday',
+	'this-month'    => 'This Month',
+	'last-month'    => 'Last Month',
+	'next-month'    => 'Next Month',
+
+	'months' => [
+	    'january'   => 'January',
+	    'february'  => 'February',
+	    'march'     => 'March',
+	    'april'     => 'April',
+	    'may'       => 'May',
+	    'june'      => 'June',
+	    'july'      => 'July',
+	    'august'    => 'August',
+	    'september' => 'September',
+	    'october'   => 'October',
+	    'november'  => 'November',
+	    'december'  => 'December',
+    ],
+
+	'days' => [
+	    'monday'    => 'Monday',
+	    'tuesday'   => 'Tuesday',
+	    'wednesday' => 'Wednesday',
+	    'thursday'  => 'Thursday',
+	    'friday'    => 'Friday',
+	    'saturday'  => 'Saturday',
+	    'sunday'    => 'Sunday',
+	],
+
+];
