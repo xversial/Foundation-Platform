ca17be7276bae3b63b9c740f387b691c8c2bc408
Bruno Gaspar <brunofgaspar1@gmail.com>
2015-01-20 21:23:02 +0000

    Update copyright headers.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- app/lang/en/message.php
+++ app/lang/en/message.php
@@ -7,13 +7,13 @@
  * Licensed under the Cartalyst PSL License.
  *
  * This source file is subject to the Cartalyst PSL License that is
- * bundled with this package in the license.txt file.
+ * bundled with this package in the LICENSE file.
  *
  * @package    Platform
  * @version    2.0.0
  * @author     Cartalyst LLC
  * @license    Cartalyst PSL
- * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @copyright  (c) 2011-2015, Cartalyst LLC
  * @link       http://cartalyst.com
  */
 
