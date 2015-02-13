fd4ab7231f7d502f9f050c72a9745bfe8aa6c60b
Suhayb Wardany <me@suw.me>
2015-02-13 04:15:12 +0200

    finish cleaning up configs
    Signed-off-by: Suhayb Wardany <me@suw.me>

--- config/platform-users.php
+++ config/platform-users.php
@@ -10,7 +10,7 @@
  * bundled with this package in the LICENSE file.
  *
  * @package    Platform Users extension
- * @version    1.0.0
+ * @version    1.0.5
  * @author     Cartalyst LLC
  * @license    Cartalyst PSL
  * @copyright  (c) 2011-2015, Cartalyst LLC
