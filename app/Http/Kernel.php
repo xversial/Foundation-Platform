9a14517bd4669b8f760f18e86b61fd3f79c55bcd
Suhayb Wardany <me@suw.me>
2015-03-04 02:30:43 +0200

    remove installer middleware
    prepending it on the installer
    
    Signed-off-by: Suhayb Wardany <me@suw.me>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends HttpKernel {
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
 		// 'App\Http\Middleware\VerifyCsrfToken',
-		'Platform\Installer\Middleware\Installer',
 	];
 
 	/**
