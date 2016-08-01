8ccda0a0f348542fa618c0ec5d1128760257974b
Bruno Gaspar <brunofgaspar1@gmail.com>
2016-08-01 13:48:30 +0100

    Update copyright headers.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- app/widgets/HelloWidget.php
+++ app/widgets/HelloWidget.php
@@ -11,10 +11,10 @@
  * bundled with this package in the LICENSE file.
  *
  * @package    Platform
- * @version    4.0.0
+ * @version    5.0.0
  * @author     Cartalyst LLC
  * @license    Cartalyst PSL
- * @copyright  (c) 2011-2015, Cartalyst LLC
+ * @copyright  (c) 2011-2016, Cartalyst LLC
  * @link       http://cartalyst.com
  */
 
