84fee8ef74024bc976222d09a1910a064ea8e0c7
Suhayb Wardany <me@suw.me>
2017-03-28 11:24:12 +0200

    only handle 404s on custom exception view
    Signed-off-by: Suhayb Wardany <me@suw.me>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,10 +44,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($pages = $this->container['platform.pages']) {
-            if ($notFoundPage = $this->container['config']->get('platform.pages.config.not_found')) {
-                if ($page = $pages->findBySlug($notFoundPage)) {
-                    return response()->make($pages->render($page));
+        if (method_exists($exception, 'getStatusCode')) {
+            if ($exception->getStatusCode() === 404) {
+                if ($pages = $this->container['platform.pages']) {
+                    if ($notFoundPage = $this->container['config']->get('platform.pages.config.not_found')) {
+                        if ($page = $pages->findBySlug($notFoundPage)) {
+                            return response()->make($pages->render($page));
+                        }
+                    }
                 }
             }
         }
